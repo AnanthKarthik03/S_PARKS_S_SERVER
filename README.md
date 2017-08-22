98a2795f7d4cfa5559395b37131775a4ff23e0f2
Vijay <vijay@akrivia.in>
2017-08-22 16:47:17 +0530

    Add /forget API for forget password

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # API
 
 ## Login & User Management
-* `/auth` - POST
-* `/profile` - GET
+`/auth`
+* Method: POST
+* Params: username, password
+
+`/profile`
+* Method: GET
+* Header: Authorization
+* Eg: `Authorization: BEARER eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwiaWF0IjoxNTAzMzk4MzE0LCJleHAiOjE1MDM0MDE5MTR9.2vgHyeRVvxb1Mtbz36AT8h-_nl_isRr_NoCQt8A8oxY`
+
+`/forget`
+* Method: POST
+* Params: username
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import Knex from './knex'
 import jwt from 'jsonwebtoken'
 const bcrypt = require('bcrypt')
+var generator = require('generate-password')
 
 const routes = [
   /* USERS */
@@ -42,6 +43,81 @@ const routes = [
     }
   },
 
+  // Forget Password
+  {
+    path: '/forget',
+    method: 'POST',
+    handler: (request, reply) => {
+      const { username } = request.payload
+      Knex('users').where({username}).then(([user]) => {
+        if (!user) {
+          reply({
+            success: false,
+            message: `Specified user doesn't exist`
+          })
+          return
+        }
+
+        let newPassword = generator.generate({
+          length: 5,
+          numbers: false
+        })
+        let hash = bcrypt.hashSync(newPassword, 10)
+
+        Knex('users')
+          .where('username', '=', username)
+          .update({
+            password: hash
+          }).then(count => {
+          if (count) {
+            const to = user.mobile
+            const msg = 'Your new password at Mitsuba is ' + newPassword
+
+            // send sms
+            if (to && msg) {
+              var request = require('request')
+              const url = 'http://login.smsmoon.com/API/sms.php'
+              const body = {
+                'username': 'raghuedu',
+                'password': 'abcd.1234',
+                'from': 'RAGHUT',
+                'to': to,
+                'msg': msg,
+                'type': '1',
+                'dnd_check': '0'
+              }
+
+              request.post(url, {
+                form: body
+              }, function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                  console.log(body) // Print the google web page.
+
+                  reply({
+                    success: true,
+                    message: 'Password update successful' + hash
+                  })
+                } else {
+                  reply({
+                    success: false,
+                    message: 'Password update successful, but sending SMS failed. Contact Administrator'
+                  })
+                }
+              })
+            }
+          } else {
+            reply({
+              success: false,
+              message: 'Password update failed'
+            })
+          }
+        })
+      }).catch((err) => {
+        reply('server-side error' + err)
+      })
+    }
+  },
+
   // Profile
   {
     path: '/profile',
@@ -52,7 +128,8 @@ const routes = [
       }
     },
     handler: (request, reply) => {
-      Knex('users').select('username', 'name', 'mobile', 'email').then((results) => {
+      let data = {username: request.auth.credentials.username}
+      Knex('users').select('username', 'name', 'mobile', 'email').where(data).then((results) => {
         if (!results || results.length === 0) {
           reply({
             error: true,
